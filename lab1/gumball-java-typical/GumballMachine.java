46366883f675eb3b51ea05311823c5eaac8224e0
xiaosayang <47075033+xiaosayang@users.noreply.github.com>
2019-02-07 06:46:41 -0800

    Update GumballMachine.java
    prompt user input

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -1,8 +1,11 @@
+import java.io.*;
+import java.util.*;
 public class GumballMachine
 {
 
     private int num_gumballs;
     private boolean has_quarter;
+    
 
     public GumballMachine( int size )
     {
@@ -21,22 +24,65 @@ public class GumballMachine
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if ( this.has_quarter )
+        {
+            if ( this.num_gumballs > 0 )
+            {
+                this.num_gumballs-- ;
+                this.has_quarter = false ;
+                System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+            }
+            else
+            {
+                System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+            }
+        }
+        else 
+        {
+            System.out.println( "Please insert a quarter" ) ;
+        }        
+    }
+    
+    public void caseDescription()
+    {
+        System.out.println( "Please chose from following Gumball Machines Models to play:\nModel1: Cost 25 cents, accepts only quarters.\nModel2: Cost 50 cents, accepts two quarters.\nModel3: Cost 50 cents, accepts all USA coins.\n");
+        System.out.println( "Enter 1 for Model1, enter 2 for Model2, enter 3 for Model 3\nEnter anything else to exit the program.\n");
+        System.out.println( "Please enter your choice: ");
+        Scanner input = new Scanner(System.in);
+        
+        while (input.hasNextInt())
+        {
+            int choice = input.nextInt();
+            if (choice == 1)
+                {
+                    System.out.println( "You have entered "+ choice);
+                    System.out.println( "Please enter your choice: ");
+                }
+           else if (choice == 2)
+                {
+                    System.out.println( "You have entered "+ choice);
+                    System.out.println( "Please enter your choice: ");
+                }
+           else if (choice == 3)
+                {
+                    System.out.println( "You have entered "+ choice);
+                    System.out.println( "Please enter your choice: ");
+                }
+           else {
+                    break;
+                }
+        }
+        System.out.println( " You have exited the program.");
+    } 
+    
+    public Dictionary createCoinDict()
+    {
+        Dictionary coins = new Hashtable();
+	coins.put(1,"Penny");
+	coins.put(5,"Nickel");
+	coins.put(10, "Dime");
+	coins.put(25, "Quarter");
+	return coins;
     }
 }
+
