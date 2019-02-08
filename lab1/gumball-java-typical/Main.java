3f5574d2a1434f8a9f09e2e3b0f96256b7241163
xiaosayang <47075033+xiaosayang@users.noreply.github.com>
2019-02-08 01:50:24 -0800

    Update Main.java
    updated Main function

--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -1,22 +1,26 @@
 
-
+import java.util.*;
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+        gumballMachine.caseDescription();
+        
+        /* origial function calls from source code
+        System.out.println(gumballMachine);
 
-		System.out.println(gumballMachine);
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
 
-		System.out.println(gumballMachine);
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-	}
+        System.out.println(gumballMachine);
+        */
+    }
 }
+
