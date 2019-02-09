c65189dda045bdf352d2a7c94dca5ade27144952
xiaosayang <47075033+xiaosayang@users.noreply.github.com>
2019-02-09 03:28:18 -0800

    Update HasQuarterState.java

--- lab1/gumball-java-pattern/HasQuarterState.java
+++ lab1/gumball-java-pattern/HasQuarterState.java
@@ -1,33 +1,47 @@
 
 
-import java.util.Random;
+import java.util.*;
 
 public class HasQuarterState implements State {
-	GumballMachine gumballMachine;
- 
-	public HasQuarterState(GumballMachine gumballMachine) {
-		this.gumballMachine = gumballMachine;
-	}
-  
-	public void insertQuarter() {
-		System.out.println("You can't insert another quarter");
-	}
- 
-	public void ejectQuarter() {
-		System.out.println("Quarter returned");
-		gumballMachine.setState(gumballMachine.getNoQuarterState());
-	}
- 
-	public void turnCrank() {
-		System.out.println("You turned...");
-		gumballMachine.setState(gumballMachine.getSoldState());
-	}
-
+    GumballMachine gumballMachine;
+    
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+    
+    public void insertQuarter(String s) {
+        System.out.println("You can not insert another quarter.");
+    }
+    
+    public void insertCoins(String s) {
+        System.out.println("You can not insert another coin.");
+    }
+    
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
+    
+    public void turnCrank() {
+        Scanner turn = new Scanner( System.in);
+        int num;
+        do {
+            System.out.println("Please enter 0 to turn the crank:");
+            while (!turn.hasNextInt()){
+                System.out.println("Please enter 0 to turn the crank:");
+                turn.next();
+            }
+            num = turn.nextInt();
+        } while ( num != 0);
+        System.out.println("You turned...");
+        gumballMachine.setState(gumballMachine.getSoldState());
+    }
+    
     public void dispense() {
         System.out.println("No gumball dispensed");
     }
- 
-	public String toString() {
-		return "waiting for turn of crank";
-	}
+     
+    public String toString() {
+        return "waiting for turn of crank";
+    }
 }
